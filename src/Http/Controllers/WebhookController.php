9f6133604ee9fbc85128e6d9afdb96d88de75510
schorpy <87610109+schorpy@users.noreply.github.com>
2025-04-02 03:25:08 +0700

    update

---
+++ src/Http/Controllers/WebhookController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Freemius\Laravel\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Str;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class WebhookController extends Controller {}
